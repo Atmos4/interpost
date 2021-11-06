a9f35c39f0bb9af8563ee336c1c8c8e09863fd5c
maximerauturier <maxime.rauturier@gmail.com>
2021-11-06 17:24:11 +0200

    strapi config

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '33013280c9022be1c43edbe542e728e7'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2292a730-59c5-4bcd-b9f7-1fa688e0c67a'
+};
